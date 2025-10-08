08722133b4276e049929d47dc99966992f9c7a75
fgastone <fabio.gastone@gmail.com>
2025-10-08 17:48:02 -0300

    Update consome.js
    mudanças no codigo para adaptar acesso ao BD não mais pelo serviceaccountkey.json mas pelo EV do vercel

--- api/consome.js
+++ api/consome.js
@@ -7,11 +7,28 @@ import fs from "fs";
 dotenv.config();
 
 // Inicializa Firebase Admin
-const serviceAccount = JSON.parse(fs.readFileSync("serviceAccountKey.json"));
-admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://diamondstd2-default-rtdb.firebaseio.com"
-});
+//const serviceAccount = JSON.parse(fs.readFileSync("serviceAccountKey.json"));
+//admin.initializeApp({
+//  credential: admin.credential.cert(serviceAccount),
+//  databaseURL: "https://diamondstd2-default-rtdb.firebaseio.com"
+//});
+
+import admin from "firebase-admin";
+
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert({
+      type: process.env.FIREBASE_TYPE,
+      project_id: process.env.FIREBASE_PROJECT_ID,
+      private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+      private_key: process.env.FIREBASE_PRIVATE_KEY,
+      client_email: process.env.FIREBASE_CLIENT_EMAIL,
+      client_id: process.env.FIREBASE_CLIENT_ID,
+      token_uri: process.env.FIREBASE_TOKEN_URI,
+    }),
+    databaseURL: "https://diamondstd2-default-rtdb.firebaseio.com",
+  });
+}
 
 const app = express();
 app.use(bodyParser.json());
